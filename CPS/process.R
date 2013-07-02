7ef7525a8229a6d74407874613646916397fe2d9
Adam Fishman <fishman.adam@gmail.com>
2013-07-02 11:37:02 -0500

    Removed stepmoms and added better explanations

--- CPS/process.R
+++ CPS/process.R
@@ -4,11 +4,6 @@ setwd("/Users/Fishface/Documents/DSSG/nfp/CPS")
 # Import Raw Data
 cps.raw <- read.table("cps.csv", sep = ',', header = TRUE)
 
-# Note that the labels for this data set will be equal to
- # "YEAR","SERIAL","HWTFINL","HWTSUPP","METRO","COUNTY","HHINCOME","PUBHOUS",
-# "RENTSUB","FOODSTMP","STAMPVAL","MONTH","WTSUPP","WTFINL","MOMLOC","POPLOC",
-#"NCHILD","ASPOUSE","AGE","SEX","RACE","MARST","BPL","EDUC","EDDIPGED",
-#"EMPSTAT","OCCLY","UHRSWORK","HIMCAID","GOTWIC","FREVER","FRBIRTHM","FRBIRTHY"
 
 # Report which statistics are available in which time periods and partition
 ## dataset into time period groups
@@ -29,13 +24,6 @@ march.2009 <- cps.raw[cps.raw$YEAR == 2009 & cps.raw$MONTH == 3,][,march.2009.la
 march.2010.labels <- which.available(3,2010)
 march.2010 <- cps.raw[cps.raw$YEAR == 2010 & cps.raw$MONTH == 3,][,march.2010.labels]
 
-june.2008.labels <- which.available(6,2008)
-june.2008 <- cps.raw[cps.raw$YEAR == 2008 & cps.raw$MONTH == 6,][,june.2008.labels]
-june.2009.labels <- which.available(6,2009)
-june.2009 <- cps.raw[cps.raw$YEAR == 2009 & cps.raw$MONTH == 6,][,june.2009.labels]
-june.2010.labels <- which.available(6,2010)
-june.2010 <- cps.raw[cps.raw$YEAR == 2010 & cps.raw$MONTH == 6,][,june.2010.labels]
-
 # Remove households that have no children under 3
 no.young.child <- function(data) {
   young.serials <- unique(data[data$AGE<3,]$SERIAL)
@@ -47,9 +35,6 @@ march.2008 <-no.young.child(march.2008)
 march.2009 <- no.young.child(march.2009)
 march.2010 <- no.young.child(march.2010)
 
-june.2008 <- no.young.child(june.2008)
-june.2009 <- no.young.child(june.2009)
-june.2010 <- no.young.child(june.2010)
 
 # Add column with combination serial and PERNUM
 per.col <- function(data) {
@@ -57,17 +42,38 @@ per.col <- function(data) {
   return(cbind(data,PERID))
 }
 
+march.2008.labels <- c(march.2008.labels, "PERID")
+march.2008 <- per.col(march.2008)
+march.2009.labels <- c(march.2009.labels, "PERID")
+march.2009 <- per.col(march.2009)
+march.2010.labels <- c(march.2010.labels, "PERID")
+march.2010 <- per.col(march.2010)
+
 #Add column with combination serial and SPLOC
 sp.col <- function(data) {
   SPID <- mapply(paste, data$SERIAL, data$SPLOC, sep = "-")
   return(cbind(data,SPID))
 }
 
+march.2008.labels <- c(march.2008.labels, "SPID")
+march.2008 <- sp.col(march.2008)
+march.2009.labels <- c(march.2009.labels, "SPID")
+march.2009 <- sp.col(march.2009)
+march.2010.labels <- c(march.2010.labels, "SPID")
+march.2010 <- sp.col(march.2010)
+
 #Add column with combination serial and POPLOC
-pop.id <- function(data) {
+pop.col <- function(data) {
   POPID <- mapply(paste, data$SERIAL, data$POPLOC, sep = "-")
   return(cbind(data, POPID))
 }
+
+march.2008.labels <- c(march.2008.labels, "POPID")
+march.2008 <- pop.col(march.2008)
+march.2009.labels <- c(march.2009.labels, "POPID")
+march.2009 <- pop.col(march.2009)
+march.2010.labels <- c(march.2010.labels, "POPID")
+march.2010 <- pop.col(march.2010)
 
 # Add column with combination serial and MOMLOC
 mom.col <- function(data) {
@@ -75,18 +81,26 @@ mom.col <- function(data) {
   return(cbind(data,MOMID))
 }
 
+march.2008.labels <- c(march.2008.labels, "MOMID")
 march.2008 <- mom.col(march.2008)
+march.2009.labels <- c(march.2009.labels, "MOMID")
 march.2009 <- mom.col(march.2009)
+march.2010.labels <- c(march.2009.labels, "MOMID")
 march.2010 <- mom.col(march.2010)
 
 # Determine whether any children younger than three have older siblings
 ## Eliminate families where all young kids have older siblings
 
 no.sibs <- function(data) {
+  # The MOMIDs of all young children (under three)
   young.momids <- unique(data[data$AGE<3,]$MOMID)
+  # The MOMIDS of older kids who share a MOMID with child under three
   sibling.momids <- unique(data[!data$AGE<3,][which(is.element(data[!data$AGE<3,]$MOMID, young.momids)),]$MOMID)
+  # The momids of young children without siblings above the age limit
   keep.momids <- young.momids[-which(is.element(young.momids,sibling.momids))]
+  # The serial numbers of the families with children we will keep
   keep.serials <- data[data$AGE<3,][which(is.element(data[data$AGE<3,]$MOMID,keep.momids)),]$SERIAL
+  # A subset of the data that contains only the applicable families
   return(data[which(is.element(data$SERIAL, keep.serials)),])
 }
 
@@ -94,29 +108,66 @@ march.2008 <- droplevels(no.sibs(march.2008))
 march.2009 <- droplevels(no.sibs(march.2009))
 march.2010 <- droplevels(no.sibs(march.2010))
 
+# Remove all families where all the young children have stepmothers
+no.step <- function(data) {
+  # For readability sake, we create a list of the relevent kids
+  kids <- data[data$AGE<3,]
+  # The serial numbers of the households where a relevent kid has no stepmom
+  no.stepmom.serials <- unique(kids[kids$STEPMOM == 0,]$SERIAL)
+  # A subset of the data that only includes the applicable families
+  return(data[which(is.element(data$SERIAL, no.stepmom.serials)),])
+}
+
+march.2008 <- droplevels(no.step(march.2008))
+march.2009 <- droplevels(no.step(march.2009))
+march.2010 <- droplevels(no.step(march.2010))
+
 # Here we add a code for whether a child under three 
 ## is the oldest child
 mark.oldest <- function(data) {
+  # Initialize n at 0, so that at the end of the function, n is the minimum number
+  ## such that there are no n-tuplets of relevent children are present
   n = 0
   result = NULL
-  line.no.first = matrix(c(1,2,3,4), nrow = 2)
+  ## Initialize line.no to pass while loop before first pass (could also use a do/while)
+  line.no = matrix(c(1,1), nrow = 1)
+  #Number the rows so that we can produce proper indexes
   LINE = 1:dim(data)[1]
   newdata <- cbind(data, LINE)
-  while(dim(line.no.first)[1] > 0) {
+  while(dim(line.no)[1] > 0) {
     n = n+1
-    line.no.first <- ddply(newdata, .(MOMID), 
+    #Prints the line numbers of the oldest relevant children
+    line.no <- ddply(newdata, .(MOMID), 
         function(df) {return(df$LINE[df$AGE == max(df$AGE) & df$AGE < 3][n])})
-    line.no.first <- line.no.first[-which(is.na(line.no.first[,2])),]
-    result = rbind(result, line.no.first)
+    #Eliminate NAs
+    line.no <- line.no[-which(is.na(line.no[,2])),]
+    result = rbind(result, line.no)
   }
   OLDEST = rep(0,dim(data)[1])
   OLDEST[result[,2]] = 1
   return(cbind(data, OLDEST))
 }
 
+march.2008.labels <- c(march.2008.labels,"OLDEST")
 march.2008 <- mark.oldest(march.2008)
+march.2009.labels <- c(march.2009.labels,"OLDEST")
 march.2009 <- mark.oldest(march.2009)
+march.2010.labels <- c(march.2010.labels,"OLDEST")
 march.2010 <- mark.oldest(march.2010)
+
+#Note that we can access the mothers of these kids [eligible mothers in the program]
+### using the lines:
+
+### These first three lines use the code above to find the MOMIDs of the children
+#### without an older sibling.
+
+#       young.momids <- unique(data[data$AGE<3,]$MOMID)
+#       sibling.momids <- unique(data[!data$AGE<3,][which(is.element(data[!data$AGE<3,]$MOMID, young.momids)),]$MOMID)
+#       keep.momids <- young.momids[-which(is.element(young.momids,sibling.momids))]
+
+### 
+#       moms <- data[which(is.element(data$PERNUM,keep.momids)),]
+
 # Coding whether an individual is working and what his/her employment status is
 ## Codes (EMPLOY):
 ######## 0 = Unemployed
@@ -167,11 +218,14 @@ march.2010 <- cbind(march.2010, EMPLOY)
 ######## 3 = No High School/GED
 hs.ged = function(data) {
   HSGED = rep(3,dim(data)[1])
-  HSGED[data$EDDIPGED == 1,] = 1
-  HSGED[data$EDDIPGED == 2,] = 2
+  HSGED[data$EDDIPGED == 1] = 1
+  HSGED[data$EDDIPGED == 2] = 2
   return(cbind(data,HSGED))}
 
+march.2008.labels <- c(march.2008.labels,"HSGED")
 march.2008 <- hs.ged(march.2008)
+march.2009.labels <- c(march.2009.labels,"HSGED")
 march.2009 <- hs.ged(march.2009)
+march.2010.labels <- c(march.2010.labels,"HSGED")
 march.2010 <- hs.ged(march.2010)
 
