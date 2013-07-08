ba1d6da478469aaf2bfea252762db8d3478362ad
eprowe <eprowe@gmail.com>
2013-07-08 14:45:39 -0500

    Added some standard data frames for uniform recode coding

--- NIS_Prep.R
+++ NIS_Prep.R
@@ -8,7 +8,7 @@
 ## I'm commenting out the source lines because 1) once they're run they don't need to run again and 2) they take too long to run.
 
 # 2008
-source("/mnt/data/NIS/original_data/nispuf08.r")
+#source("/mnt/data/NIS/original_data/nispuf08.r")
 #NISPUF08 <- subset(NISPUF08, PDAT==1)
 #save("NISPUF08", "/mnt/data/NIS/modified_data/NISPUF08.RData")
 
@@ -17,8 +17,24 @@ source("/mnt/data/NIS/original_data/nispuf08.r")
 #source("/mnt/data/NIS/original_data/nispuf10.r")
 #source("/mnt/data/NIS/original_data/nispuf11.r")
 
+# Load NIS data for recoding and preparation
+
+setwd("/mnt/data/modified_data")
+NIS08 <- load("NISPUF08.RData")
+NIS09 <- load("NISPUF09.RData")
+NIS10 <- load("NISPUF10.RData")
+NIS11 <- load("NISPUF11.RData")
+
+# Variable Recodes - NIS
 
 
 
 
 
+# Load NFP data for recoding and preparation
+
+setwd("/mnt/data/csv_data")
+nfp_demographics <- read.csv("nfp_demographics_expanded.csv")
+nfp_centers <- read.csv("agency.csv")
+
+# Variable Recodes - NFP
