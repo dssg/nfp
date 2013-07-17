8eb97cc813c52258f37dddeee42171f56cb4d264
eprowe <eprowe@gmail.com>
2013-07-17 17:29:44 -0500

    Added more recodes to NSCH prep file

--- NSCH/NSCH_Prep.R
+++ NSCH/NSCH_Prep.R
@@ -4,7 +4,7 @@ nfpdemo <- read.csv("nfp_demographics_expanded.csv")
 setwd("/mnt/data/NSCH/data")
 nsch <- read.csv("DRC_2011_2012_NSCH.csv")
 
-# Recoding sex, prematurity, low birthweight variables from NSCH to match NFP
+# Recoding sex, prematurity, low birthweight, education, marital status variables from NSCH to match NFP
 nsch$NSCH_ID <- nsch$IDNUMR
 nsch$male[nsch$SEX==2] <- 0 # Male binary = 0 if child is female; null if DK/refused
 nsch$male[nsch$SEX==1] <- 1 
@@ -12,6 +12,11 @@ nsch$premature[nsch$K2Q05==0] <- 0 # Indicator for prematurity; null if DK/refus
 nsch$premature[nsch$K2Q05==1] <- 1
 nsch$lbw[nsch$ind1_8_11==1] <- 1 # Indicator for low birthweight; null if DK/refused
 nsch$lbw[nsch$ind1_8_11==2] <- 0
+nsch$highschool[nsch$EDUC_MOMR >= 3] <- 1 # Indicator for whether mother graduated from high school
+nsch$highschool[nsch$EDUC_MOMR <= 2] <- 0
+nsch$highered[nsch$EDUC_MOMR >= 5] <- 1
+nsch$highered[nsch$EDUC_MOMR < 5] <- 0
+# nsch$marital_status (use FAM_MAR_COHAB)
 
 # Recoding age from NSCH to match NFP
 nsch$MomsAge <- nsch$K9Q16R 
@@ -21,21 +26,12 @@ nsch$MomsAgeBirth <- nsch$MomsAge - nsch$AGEYR_CHILD # Mother's age at birth = m
 # Shouldn't be an issue for our analysis since we are only working with infants/toddlers
 # Highlight some examples for question: subset(nsch, nsch$MomsAgeBirth<=15, select = c(NSCH_ID, K9Q16R, MomsAge, AGEYR_CHILD, MomsAgeBirth))
 
-# Recoding education from NSCH to match information available from NFP
-nsch$momsEd[nsch$EDUC_MOMR==1] <- "NoHS" # 8th grade education or less
-nsch$momsEd[nsch$EDUC_MOMR==2] <- "HSnoDip" # Last year of schooling was 9th-12th
-nsch$momsEd[which(is.element(nsch$EDUC_MOMR,c(3,6,7)))] <- "HSorGED" # Received at least HS degree or GED
-nsch$momsEd <- factor(nsch$momsEd)
-
-# Recoding ID, sex, education from NFP
+# Recoding ID, sex from NFP
 nfpdemo$NFP_ID <- nfpdemo$CL_EN_GEN_ID
+# nfpdemo$highered <- nfpdemo$client_higher_educ_1 # Check this - may not be working
 nfpdemo$male[nfpdemo$Childgender=="Female"] <- 0 # Recode factor variable
 nfpdemo$male[nfpdemo$Childgender=="Male"] <- 1 
 
-nfpdemo$momsEd[nfpdemo$HSGED_Last_Grade_1 <= 8] <- "NoHS"
-nfpdemo$momsEd[nfpdemo$HSGED_Last_Grade_1 > 8] <- "HSnoDip"
-nfpdemo$momsEd[which(is.element(nfpdemo$HSGED, c(1,2)))] <- "HSorGED"
-nfpdemo$momsEd <- factor(nfpdemo$momsEd)
 
 # Race recodes: note that NSCH has only child's race and NFP has only mother's race.
 # Must assume for matching purposes that they are the same.
@@ -54,13 +50,12 @@ nfpdemo$RE[nfpdemo$RE=="Hispanic or Latina"] <- "Hispanic" # Shortening descript
 nfpdemo$RE[nfpdemo$RE=="Declined or msg"] <- NA 
 nfpdemo$RE <- factor(nfpdemo$RE) # Return to factor format with adjusted levels
 
-
-# Remove extra NSCH columns and obs for families with no children < age 2.
-NSCH_Columns <- subset(nsch, select = c(NSCH_ID, male, premature, lbw, RE))
-NSCH_Final <- subset(nsch) # Subset here by child's age
+# Remove extra NSCH columns, obs for families with no children < age 2, families making more than 200% of FPL
+# (WIC/NFP criteria is generally 100-185% FPL, so these mothers are not valid matches)
+NSCH_Final <- subset(nsch, nsch$POVLEVEL_I <= 2, select = c(NSCH_ID, male, premature, lbw, RE))
+# Subset here by child's age
 
 # Remove extra NFP columns.
-NFP_Demo_Set <- subset(nfpdemo, select = c(NFP_ID, male, premature, lbw, RE))
-NFP_Final <- merge(NFP_Demo_Set, NFP_Locaton) # Merge on location information
+NFP_Final <- subset(nfpdemo, select = c(NFP_ID, male, premature, lbw, RE))
 
 # Combine final datasets to create analysis dataset.
